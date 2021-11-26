241bbe511d8e9372390016b2bcbd5d2ce38f63b1
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-26 03:04:01 +0100

    ...

---
+++ BeansApp/src/main/java/africa/semicolon/BeansApp/exceptions/BeansAppException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.BeansApp.exceptions;
+
+public class BeansAppException extends RuntimeException {
+    public BeansAppException(String message) {
+        super(message);
+    }
+}
