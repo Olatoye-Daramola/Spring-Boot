b0f25b65066f1d55cb1ad7182127ecaf64d9ed37
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-26 03:04:33 +0100

    ...

---
+++ BeansApp/src/main/java/africa/semicolon/BeansApp/data/models/Customer.java
@@ -0,0 +1,10 @@
+package africa.semicolon.BeansApp.data.models;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private String email;
+    private String fullName;
+    private String password;
+}
