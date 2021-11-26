32c2b746b474563cdae23941145724831b946daa
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-26 03:15:37 +0100

    ...

---
+++ JijiClone/src/main/java/africa/semicolon/jijiClone/dtos/responses/RegisterListerResponse.java
@@ -0,0 +1,9 @@
+package africa.semicolon.jijiClone.dtos.responses;
+
+import lombok.Data;
+
+@Data
+public class RegisterListerResponse {
+    private String dbId;
+    private String dateAndTimeOfCreation;
+}
