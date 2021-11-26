1638424a2a5e0d57ea9004b95737e53017a31cdb
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-26 03:04:49 +0100

    ...

---
+++ BankingApplication/src/main/java/africa/semicolon/BankingApplication/data/repositories/CustomerRepository.java
@@ -0,0 +1,13 @@
+package africa.semicolon.BankingApplication.data.repositories;
+
+import africa.semicolon.BankingApplication.data.models.Customer;
+
+import java.util.List;
+
+public interface CustomerRepository {
+    Customer save(Customer customer);
+    Customer findCustomerById(String id);
+    void delete(Customer customer);
+    void delete(String id);
+    List<Customer> findAll();
+}
