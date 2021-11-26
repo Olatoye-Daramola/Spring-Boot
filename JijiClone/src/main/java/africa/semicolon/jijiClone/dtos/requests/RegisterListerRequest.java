f346c45381748b0b52b4dd6133c0740cf6b60bee
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-26 02:50:24 +0100

    ...

---
+++ JijiClone/src/main/java/africa/semicolon/jijiClone/dtos/requests/RegisterListerRequest.java
@@ -0,0 +1,13 @@
+package africa.semicolon.jijiClone.dtos.requests;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RegisterListerRequest {
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String location;
+}
