16d10f2648fa0cd1d611b469b5fe56c313570bd2
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-26 02:48:09 +0100

    ...

---
+++ JijiClone/src/main/java/africa/semicolon/jijiClone/exceptions/DuplicateEmailException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.jijiClone.exceptions;
+
+public class DuplicateEmailException extends JijiCloneAppException{
+    public DuplicateEmailException(String message) {
+        super(message);
+    }
+}
