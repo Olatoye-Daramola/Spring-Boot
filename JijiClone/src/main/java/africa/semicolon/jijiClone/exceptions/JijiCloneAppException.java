44fd75ee4f63b20e74d8fc4042a1871dc28d2bc5
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-26 02:48:24 +0100

    ...

---
+++ JijiClone/src/main/java/africa/semicolon/jijiClone/exceptions/JijiCloneAppException.java
@@ -0,0 +1,8 @@
+package africa.semicolon.jijiClone.exceptions;
+
+public class JijiCloneAppException extends RuntimeException{
+
+    public JijiCloneAppException(String message) {
+        super(message);
+    }
+}
