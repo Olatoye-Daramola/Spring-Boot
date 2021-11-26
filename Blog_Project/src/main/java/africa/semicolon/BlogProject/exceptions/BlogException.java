ef3d11a9073e58967ab3af722f4a71ea695e46ca
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-26 02:46:07 +0100

    ...

---
+++ Blog_Project/src/main/java/africa/semicolon/BlogProject/exceptions/BlogException.java
@@ -0,0 +1,18 @@
+package africa.semicolon.BlogProject.exceptions;
+
+public class BlogException extends RuntimeException{
+    public BlogException() {
+    }
+
+    public BlogException(String message) {
+        super(message);
+    }
+
+    public BlogException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BlogException(Throwable cause) {
+        super(cause);
+    }
+}
