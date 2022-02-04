a5407a0b29339ad11c3033e8114b1f6ebf51d110
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-04 10:00:27 +0100

    ...

---
+++ phoenix/src/main/resources/db/insert.sql
@@ -0,0 +1,7 @@
+truncate table product;
+
+insert into product (id, name, price, quantity)
+values(12, 'Luxury Map', 2340, 3),
+      (13, 'Macbook Air', 1340, 4),
+      (14, 'Rocking chair', 4140, 5),
+      (15, 'Purple T-shirt', 6380, 7)
