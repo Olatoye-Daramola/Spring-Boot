2d51c7509a4b0c36bd291051f51a48fb6c100e22
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-04 10:01:08 +0100

    ...

---
+++ phoenix/src/main/java/africa/semicolon/phoenix/web/exceptions/ProductDoesNotExistException.java
@@ -0,0 +1,8 @@
+package africa.semicolon.phoenix.web.exceptions;
+
+public class ProductDoesNotExistException extends Throwable {
+
+    public ProductDoesNotExistException(String s) {
+        super(s);
+    }
+}
