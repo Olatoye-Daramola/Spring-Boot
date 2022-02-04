6a5d42ac9681628e97435751d9c42c2348d1a7e1
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-04 10:03:04 +0100

    ...

---
+++ phoenix/src/main/resources/db/setup.sql
@@ -0,0 +1,6 @@
+--                             MySql Setup
+-- create database phoenix_db;
+--
+-- create user 'phoenix_user'@'localhost' identified by 'pass_123';
+-- grant all privileges on phoenix_db.* to 'phoenix_user'@'localhost';
+-- flush privileges;
