34e3493046e48044bd792b1f8e689aa848d1a07e
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-04 10:37:34 +0100

    ...

---
+++ phoenix/src/main/java/africa/semicolon/phoenix/web/exceptions/BusinessLogicException.java
@@ -0,0 +1,8 @@
+package africa.semicolon.phoenix.web.exceptions;
+
+public class BusinessLogicException extends Throwable {
+
+    public BusinessLogicException(String s) {
+        super(s);
+    }
+}
