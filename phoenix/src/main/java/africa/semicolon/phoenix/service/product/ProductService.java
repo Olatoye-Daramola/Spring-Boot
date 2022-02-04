99cf097a57d1634bcb33dddd433e5df6d1fe3699
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-04 10:01:54 +0100

    ...

---
+++ phoenix/src/main/java/africa/semicolon/phoenix/service/product/ProductService.java
@@ -0,0 +1,16 @@
+package africa.semicolon.phoenix.service.product;
+
+import africa.semicolon.phoenix.data.dto.ProductDto;
+import africa.semicolon.phoenix.data.models.Product;
+import africa.semicolon.phoenix.web.exceptions.BusinessLogicException;
+import africa.semicolon.phoenix.web.exceptions.ProductDoesNotExistException;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAllProducts();
+    Product findProductById(Long productId) throws ProductDoesNotExistException;
+    Product createProduct(ProductDto productDto) throws BusinessLogicException;
+    Product updateProductById(Long productId, Product product) throws ProductDoesNotExistException;
+}
