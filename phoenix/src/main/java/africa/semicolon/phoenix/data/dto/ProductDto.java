4ab4b2a1d101b689b7468165dd63bf75f517d397
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-04 10:01:19 +0100

    ...

---
+++ phoenix/src/main/java/africa/semicolon/phoenix/data/dto/ProductDto.java
@@ -0,0 +1,16 @@
+package africa.semicolon.phoenix.data.dto;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import java.util.Date;
+
+@Data
+public class ProductDto {
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+    private String imageUrl;
+}
