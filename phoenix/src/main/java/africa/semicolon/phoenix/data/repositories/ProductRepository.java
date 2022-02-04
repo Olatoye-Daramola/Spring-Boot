8b671902f3460e5ec106962bf058edf67ccf4255
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-04 10:00:56 +0100

    ...

---
+++ phoenix/src/main/java/africa/semicolon/phoenix/data/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package africa.semicolon.phoenix.data.repositories;
+
+import africa.semicolon.phoenix.data.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+}
