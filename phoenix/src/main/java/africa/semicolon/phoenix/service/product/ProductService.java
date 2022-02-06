4acd6c3b62298e1c82bddad2c9a912bc0ba32cbc
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-06 15:56:39 +0100

    ProductService

--- phoenix/src/main/java/africa/semicolon/phoenix/service/product/ProductService.java
+++ phoenix/src/main/java/africa/semicolon/phoenix/service/product/ProductService.java
@@ -4,6 +4,9 @@ import africa.semicolon.phoenix.data.dto.ProductDto;
 import africa.semicolon.phoenix.data.models.Product;
 import africa.semicolon.phoenix.web.exceptions.BusinessLogicException;
 import africa.semicolon.phoenix.web.exceptions.ProductDoesNotExistException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 
 import java.util.List;
 
@@ -12,5 +15,8 @@ public interface ProductService {
     List<Product> getAllProducts();
     Product findProductById(Long productId) throws ProductDoesNotExistException;
     Product createProduct(ProductDto productDto) throws BusinessLogicException;
-    Product updateProductById(Long productId, ProductDto productDto) throws ProductDoesNotExistException;
+//    Product updateProductById(Long productId, ProductDto productDto) throws ProductDoesNotExistException;
+    Product saveOrUpdate(Product product) throws BusinessLogicException;
+    Product updateProductDetails(Long productId, JsonPatch patch)
+            throws ProductDoesNotExistException, BusinessLogicException, JsonPatchException, JsonProcessingException;
 }
