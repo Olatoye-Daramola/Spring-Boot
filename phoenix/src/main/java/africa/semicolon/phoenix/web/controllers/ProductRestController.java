4720ec9ac98c0430eb335f550f7dc396a76ee1de
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-06 15:55:50 +0100

    ProductRestController

--- phoenix/src/main/java/africa/semicolon/phoenix/web/controllers/ProductRestController.java
+++ phoenix/src/main/java/africa/semicolon/phoenix/web/controllers/ProductRestController.java
@@ -5,7 +5,11 @@ import africa.semicolon.phoenix.data.models.Product;
 import africa.semicolon.phoenix.service.product.ProductService;
 import africa.semicolon.phoenix.web.exceptions.BusinessLogicException;
 import africa.semicolon.phoenix.web.exceptions.ProductDoesNotExistException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,13 +38,24 @@ public class ProductRestController {
         }
     }
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
+//    @PutMapping("/{id}")
+//    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
+//        try {
+//            Product updatedProduct = productService.updateProductById(id, productDto);
+//            return ResponseEntity.ok().body(updatedProduct);
+//        }  catch (IllegalArgumentException | ProductDoesNotExistException e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        }
+//    }
+
+    @PatchMapping(path = "/{id}", consumes = "application/json-patch+json")
+    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody JsonPatch patch) {
         try {
-            Product updatedProduct = productService.updateProductById(id, productDto);
+            Product updatedProduct = productService.updateProductDetails(id, patch);
             return ResponseEntity.ok().body(updatedProduct);
-        }  catch (IllegalArgumentException | ProductDoesNotExistException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        catch (BusinessLogicException | JsonPatchException | JsonProcessingException | ProductDoesNotExistException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 }
