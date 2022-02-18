db932551e1c4db7d34a640997f7621e8509130a8
Olatoye Daramola <daramolatoye@gmail.com>
2022-02-18 16:42:19 +0100

    ...

---
+++ EmployeeManager/src/main/java/africa/semicolon/employeemanager/service/cloud/CloudService.java
@@ -0,0 +1,11 @@
+package africa.semicolon.employeemanager.service.cloud;
+
+import java.io.IOException;
+import java.util.Map;
+
+public interface CloudService {
+
+//    Map<?,?> upload(File file, Map<?,?> params) throws IOException;
+//    Map<?,?> upload(MultipartFile file, Map<?,?> params) throws IOException;
+    Map<?,?> upload(byte[] bytes, Map<?,?> params) throws IOException;
+}
